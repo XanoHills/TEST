318d9920be49c789d75e307634f6f2383797d023
Xano Hills <sshow8720@gmail.com>
2023-12-27 15:06:50 +0700

    Вынес проверку в отдельную функцию, исправил взаимодействие с консолью

--- shades_of_gray.py
+++ shades_of_gray.py
@@ -1,50 +1,46 @@
+import sys
 from typing import Tuple
 
-def is_gray(hex_color: str) -> bool:
-    # Проверка на корректный формат ввода
-    if not hex_color.startswith("#") or len(hex_color) != 7 or not all(c.upper() in "0123456789ABCDEF" for c in hex_color[1:]):
-        print("Некорректный формат ввода. Пожалуйста, введите цвет в формате '#HHHHHH'.")
-        return False
 
+def input_mistake(hex_color):
+    return (not hex_color.startswith("#") or len(hex_color) != 7
+            or not all(c.upper() in "0123456789ABCDEF" for c in hex_color[1:]))
+
+
+def is_gray(hex_color: str) -> bool:
     # Извлекаем значения цветовых компонент
     r, g, b = int(hex_color[1:3], 16), int(hex_color[3:5], 16), int(hex_color[5:7], 16)
 
     # Проверяем, является ли цвет серым
     return r == g == b
 
-def to_gray(hex_color: str) -> Tuple[int, int, int]:
-    # Проверка на корректный формат ввода
-    if not hex_color.startswith("#") or len(hex_color) != 7 or not all(c.upper() in "0123456789ABCDEF" for c in hex_color[1:]):
-        print("Некорректный формат ввода. Пожалуйста, введите цвет в формате '#HHHHHH'.")
-        return (0, 0, 0)
 
+def to_gray(hex_color: str) -> Tuple[int, int, int]:
     # Извлекаем значения цветовых компонент
     r, g, b = int(hex_color[1:3], 16), int(hex_color[3:5], 16), int(hex_color[5:7], 16)
 
     # Вычисляем значения, которые нужно прибавить к каждой компоненте, чтобы сделать цвет серым
     delta_r, delta_g, delta_b = 128 - r, 128 - g, 128 - b
 
-    return (delta_r, delta_g, delta_b)
+    return delta_r, delta_g, delta_b
 
-# Меню программы
+
 while True:
-    print("\nМеню:")
-    print("1. Проверить, является ли цвет серым")
-    print("2. Получить значения для преобразования цвета в серый")
-    print("3. Выход")
+    hex_color = input("'0' для выхода\n"
+                      "Введите цвет в формате '#HHHHHH', \n"
+                      "где H - любой символ шестнадцатеричного кода (от 0 до F): ")
+    hex_color = hex_color.replace(' ', '')
 
-    choice = input("Выберите действие (1, 2, 3): ")
+    if hex_color == '0':
+        sys.exit("\nВыход из программы")
 
-    if choice == "1":
-        hex_color = input("Введите цвет в формате '#HHHHHH': ")
-        result = is_gray(hex_color)
-        print(f"Цвет {hex_color} является серым: {result}")
-    elif choice == "2":
-        hex_color = input("Введите цвет в формате '#HHHHHH': ")
+    if input_mistake(hex_color):
+        print("\nНекорректный формат ввода. Пожалуйста, введите цвет в формате '#HHHHHH'.\n")
+        continue
+
+    if not is_gray(hex_color):
         result = to_gray(hex_color)
         print(f"Для преобразования цвета {hex_color} в серый добавьте к каждой компоненте: {result}")
-    elif choice == "3":
-        print("Выход из программы.")
-        break
+
     else:
-        print("Некорректный выбор. Пожалуйста, выберите 1, 2 или 3.")
+        print(f"Цвет {hex_color} является серым.")
